d7e31cd06f6b9c9c50c23a6a08f64b64e512d8fc
Urgaar <granier.sylvain84@gmail.com>
2022-03-21 11:13:17 +0100

    structure created

---
+++ ui.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Première application"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("choix", "Abscisse", c("wt", "hp", "disp")),
+      textOutput("resumeMpg"),
+      textOutput("resumeVar"),
+      tableOutput("tableau")
+    ),
+    mainPanel(
+      plotOutput("nuage")
+    )
+  )
+))
+
