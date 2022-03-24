ebd3576b8f56618115330e97aec163e443695240
Kirill Makarov <68103731+luaroncrew@users.noreply.github.com>
2022-03-24 09:01:50 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,39 +1,8 @@
+
 library(shiny)
 
-shinyServer(function(input, output) {
-  output$nuage <- renderPlot({
-    plot(mtcars$mpg ~ mtcars[,input$choix], main = "Consommation")
-  })
-  
-  output$resumeMpg <- renderText({
-    paste("Moyenne mpg :", mean(mtcars$mpg, na.rm = T))
-  })
-  output$resumeVar <- renderText({
-    paste("Moyenne", input$choix, ":", mean(mtcars[,input$choix], na.rm = T))
-  })
-  
-  output$tableau <- renderTable({
-    sub = subset(mtcars, select = c("mpg", input$choix))
-    sapply(sub, summary)
-  })
-})
+dataset <- read.csv('tel.csv', sep=";", dec=".")
 
-
-quantiQuanti <-function(variable1,variable2){
-  print(ggplot(df, aes(x=variable1, y=variable2)) + geom_point())  
-  coeff_corel <- cor(x = variable1, y = variable2,use = "complete.obs")
-}
-
-  
-qualiQuali <- function(varname_1,varname_2){
-  tab <- cprop(table(variable1,variable2))
-  print(barplot(tab))
-}
-  
-
-QualiQuanti <- function(varname_1,varname_2){
-  return(boxplot(varname_1 ~ varname_2))
-}
 lien2Variables <- function(varname_1,varname_2){
   if (is.numeric(varname_1) & is.numeric(varname_2))
   {
@@ -48,4 +17,36 @@ lien2Variables <- function(varname_1,varname_2){
     return(qualiQuali(varname_1,varname_2))
   }
   
+}
+
+
+quantiQuanti <-function(variable1,variable2){
+  return(ggplot(df, aes(x=variable1, y=variable2)) + geom_point())  
+  coeff_corel <- cor(x = variable1, y = variable2,use = "complete.obs")
+}
+
+
+qualiQuali <- function(varname_1,varname_2){
+  tab <- cprop(table(variable1,variable2))
+  return(barplot(tab))
+}
+
+
+QualiQuanti <- function(varname_1,varname_2){
+  return(boxplot(varname_1 ~ varname_2))
+}
+
+
+
+# Define a server for the Shiny app
+function(input, output) {
+  dataset <- read.csv('tel.csv', sep=";", dec=".")
+  varname_1 = dataset[,input$varname_1]
+  varname_2 = dataset[,input$varname_2]
+  
+  
+  # Fill in the spot we created for a plot
+  output$telplot <- renderPlot({
+    comparison_choice = lien2Variables(varname_1, varname_2)
+  })
 }
