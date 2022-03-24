176ae0134ce07d9cfdc0cb819c7c1a478556d2e2
Kirill Makarov <68103731+luaroncrew@users.noreply.github.com>
2022-03-24 09:27:14 +0100

    maintenant ça marche bitch

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
-
+library(ggplot2)
 library(shiny)
+library(questionr)
 
 dataset <- read.csv('tel.csv', sep=";", dec=".")
 
@@ -21,14 +22,13 @@ lien2Variables <- function(varname_1,varname_2){
 
 
 quantiQuanti <-function(variable1,variable2){
-  return(ggplot(df, aes(x=variable1, y=variable2)) + geom_point())  
+  return(plot(variable1, variable2))
   coeff_corel <- cor(x = variable1, y = variable2,use = "complete.obs")
 }
 
 
 qualiQuali <- function(varname_1,varname_2){
-  tab <- cprop(table(variable1,variable2))
-  return(barplot(tab))
+  return(barplot(table(varname_1, varname_2)))
 }
 
 
@@ -40,13 +40,11 @@ QualiQuanti <- function(varname_1,varname_2){
 
 # Define a server for the Shiny app
 function(input, output) {
-  dataset <- read.csv('tel.csv', sep=";", dec=".")
-  varname_1 = dataset[,input$varname_1]
-  varname_2 = dataset[,input$varname_2]
-  
-  
   # Fill in the spot we created for a plot
   output$telplot <- renderPlot({
-    comparison_choice = lien2Variables(varname_1, varname_2)
+    comparison_choice = lien2Variables(dataset[,input$varname_1],
+                                       dataset[,input$varname_2])
   })
 }
+
+# corrige mon code toi connard
